8a65ba5771156fa883510fcb3106efb4a6c5c0ae
Himanshu Kumar <himanshukr433@gmail.com>
2024-02-23 13:09:56 +0530

    Revert "first commit"
    This reverts commit 9ac34bdbd7f56f3070f74b3a12eede6ff5efd984.

--- lib/consts/consts.dart
+++
@@ -1,8 +0,0 @@
-export './colors.dart';
-export './images.dart';
-export './strings.dart';
-export './styles.dart';
-export 'package:velocity_x/velocity_x.dart';
-export 'package:flutter/material.dart';
-export './firebase_consts.dart';
-
--- lib/widgets_common/loading_indicator.dart
+++
@@ -1,7 +0,0 @@
-import 'package:emart_app/consts/consts.dart';
-
-Widget loadingIncicatior(){
-  return const CircularProgressIndicator(
-    valueColor: AlwaysStoppedAnimation(redColor),
-  );
-}
